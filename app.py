7d2b5e13a10833b9d78ce889e846c2cf80472d25
Alberto Gonzalez <albertogonzalez@Albertos-MacBook-Air.local>
2021-06-14 11:17:50 -0400

    coding python file for routes

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from flask import Flask, jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -8,3 +10,23 @@ from sqlalchemy import create_engine, func
 # Database Setup
 #################################################
 engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+# Using the query from part 1 (most recent 12 months of precipitation data), convert the query results to a dictionary 
+# using date as the key and prcp as the value.
+# Return the JSON representation of your dictionary (note the specific format of your dictionary as required from above).
+@app.route("/api/v1.0/precipitation")
+def precip():
