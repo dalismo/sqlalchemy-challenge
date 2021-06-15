364c27fe498fe8f06e03150549d11490f8cdfa2b
Alberto Gonzalez <albertogonzalez@Albertos-MacBook-Air.local>
2021-06-15 11:14:44 -0400

    working on step 2

--- app.py
+++ app.py
@@ -1,15 +1,17 @@
 import numpy as np
+# import pandas as pd
+# import datetime as dt
 
 from flask import Flask, jsonify
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session, session
 from sqlalchemy import create_engine, func
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -17,11 +19,30 @@ Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
 
-#################################################
-# Flask Setup
-#################################################
+# view all the classes that automap found
+Base.classes.keys()
+
+#Save references to each table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# #################################################
+# # Flask Setup
+# #################################################
 app = Flask(__name__)
 
+# #Define the routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
 #################################################
 # Flask Routes
 #################################################
@@ -29,4 +50,23 @@ app = Flask(__name__)
 # using date as the key and prcp as the value.
 # Return the JSON representation of your dictionary (note the specific format of your dictionary as required from above).
 @app.route("/api/v1.0/precipitation")
-def precip():
+def precipitation():
+    prcp_results=session.query(Measurement.date, func.sum(Measurement.prcp)).group_by(Measurement.date).\
+        order_by(Measurement.id.desc().limit(365).all()
+
+        prcp_results_dict = {}
+        for date, prcp in prcp_results:
+            if prcp !=None:
+
+
+# Dictionary of TOBS Data
+    # """Return a list of dates and tobs"""
+    # """Query for the dates and temperature observations from the last year.
+    # Convert the query results to a Dictionary using date as the key and tobs as the value.
+    # Return the JSON representation of your dictionary."""
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
